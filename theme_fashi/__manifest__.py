28dccebd8820b0800e23919b7bcd786f5285185c
fady <fady.noor@gmail.com>
2020-02-12 08:32:58 +0700

    [ADD] theme_fashi: add scss from fashi html

---
+++ theme_fashi/__manifest__.py
@@ -0,0 +1,15 @@
+{  
+  'name':'Fashi',
+  'description': 'theme build based on Fashi theme from Colorlib',
+  'version':'1.0',
+  'author':'Your name',
+
+  'data': [
+      'views/assets.xml',
+      # 'views/layout.xml',
+      # 'views/pages.xml',
+      
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_blog', 'website_sale'],
+}
